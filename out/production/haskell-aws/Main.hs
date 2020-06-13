80cbe778184fc565d79101775e6db68e2d667b80
Christoph Welcz <christoph@welcz.de>
2020-06-13 08:33:01 +0200

    :sparkles: calculating fizz buzz sequence

--- out/production/haskell-aws/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import Aws.Lambda
-
-import qualified Lib
-
-generateLambdaDispatcher
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,15 +1,19 @@
 module Lib where
 
-import           Data.Aeson
-import           GHC.Generics
-
 import           Aws.Lambda
-import           Data.HashMap.Lazy (HashMap)
-import qualified Data.HashMap.Lazy as HashMap
+import           Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as ByteString
+import           Data.HashMap.Lazy          (HashMap)
+import qualified Data.HashMap.Lazy          as HashMap
+import           Data.List                  (intercalate)
+import           GHC.Generics
+import           Text.Read                  (readMaybe)
+
+type PathParameters = HashMap String String
 
 data Event = Event
     { resource       :: String
-    , pathParameters :: Maybe (HashMap String String)
+    , pathParameters :: PathParameters
     }
     deriving (Generic, FromJSON)
 
@@ -20,5 +24,30 @@ data Response = Response
     deriving (Generic, ToJSON)
 
 handler :: Event -> Context -> IO (Either String Response)
-handler Event{..} context =
-  pure $ Right $ Response { statusCode = 200, body = "Success" }
+handler Event {..} context =
+  pure $ Right $ Response {statusCode = 200, body = (toBody . fmap sequenceUpTo) limit}
+  where
+    limit = extractLimit pathParameters >>= validateLimit
+
+extractLimit :: PathParameters -> Maybe Int
+extractLimit params = HashMap.lookup "input" params >>= readMaybe
+
+validateLimit :: Int -> Maybe Int
+validateLimit n
+  | n < 0 = Nothing
+  | n > 10000 = Nothing
+  | otherwise = Just n
+
+single :: Int -> String
+single n = if result == "" then show n else result
+  where
+    result = intercalate "-" [result | (divisor, result) <- rules, n `mod` divisor == 0]
+    rules = [(3, "Fizz"), (5, "Buzz")]
+
+sequenceUpTo :: Int -> [String]
+sequenceUpTo limit = single <$> [1..limit]
+
+toBody :: ToJSON a => Maybe a -> String
+toBody Nothing   = ""
+toBody (Just xs) = (ByteString.unpack . encode) xs
+
